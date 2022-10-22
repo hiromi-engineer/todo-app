698c6386565a0363fae52d555a5ea63bc8aeebcc
hiromi.exceed@gmail.com <hiromi.exceed@gmail.com>
2022-10-22 23:48:28 +0900

    TODOの編集機能、ステータスでのフィルター機能追加

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ const App = () => {
   const [newTodoTitle, setNewTodoTitle] = useState('');
   const [newTodoDesc, setNewTodoDesc] = useState('');
   const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState({});
+  const [filterStatus, setFilterStatus] = useState('全て');
   const createUUID = () => {
     const uuid = "xxxxxxxx-xxxx-4xxx-Zxxx-xxxxxxxxxxxx".split("");
     const len = uuid.length;
@@ -25,6 +27,7 @@ const App = () => {
     }
     return uuid.join("");
   }
+  // todoの追加
   const changeNewTodoTitle = e => {
     setNewTodoTitle(e.target.value);
   }
@@ -41,6 +44,11 @@ const App = () => {
       document.getElementById('add-error').innerText = 'タイトルを入力してください';
     }
   }
+  // filter
+  const changeFilterStatus = e => {
+    setFilterStatus(e.target.value);
+  }
+  // todoの編集
   const changeStatus = (id, newStatus) => {
     const newTodos = todos.map( todo => todo.id === id ? {...todo, status: newStatus} : todo );
     setTodos(newTodos);
@@ -49,6 +57,28 @@ const App = () => {
     const newTodos = todos.filter( todo => todo.id !== id );
     setTodos(newTodos);
   }
+  const openEditor = todo => {
+    setEditTodo(todo);
+  }
+  const changeEditTodoTitle = e => {
+    setEditTodo({...editTodo, title: e.target.value});
+  }
+  const changeEditTodoDesc = e => {
+    setEditTodo({...editTodo, desc: e.target.value});
+  }
+  const completeEditTodo = () => {
+    if(editTodo.title) {
+      const newTodos = todos.map( todo => todo.id === editTodo.id ? editTodo : todo );
+      setTodos(newTodos);
+      setEditTodo({});
+      document.getElementById('edit-error').innerText = '';
+    } else {
+      document.getElementById('edit-error').innerText = 'タイトルを入力してください';
+    }
+  }
+  const closeEditor = () => {
+    setEditTodo({});
+  }
   return (
     <>
       <h1>TODO</h1>
@@ -56,21 +86,41 @@ const App = () => {
       <textarea name="newTodoDesc" value={newTodoDesc} placeholder="詳細" onChange={changeNewTodoDesc}></textarea><br />
       <button onClick={addNewTodo}>追加</button>
       <h2>一覧</h2>
+      <span>表示するステータス：</span>
+      <select name="status" value={filterStatus} onChange={changeFilterStatus}>
+        <option value="全て">全て</option>
+        <option value="未着手">未着手</option>
+        <option value="進行中">進行中</option>
+        <option value="完了">完了</option>
+      </select>
       <ul>
-        {
-          todos.map(todo => {
-            return (
-              <li>
-                <h3 style={{display: 'inline-block', margin: '10px 10px 10px 0'}}>{todo.title}</h3>
-                <select name="status" value={todo.status} onChange={e => changeStatus( todo.id, e.target.value )}>
-                  <option value="未着手">未着手</option>
-                  <option value="進行中">進行中</option>
-                  <option value="完了">完了</option>
-                </select><br />
-                {todo.desc && <p style={{margin: '0 0 10px 0'}}>{todo.desc}</p>}
-                <button onClick={() => deleteTodo(todo.id)}>削除</button>
-              </li>
-            );
+        { todos.map(todo => {
+            if( filterStatus === '全て' || filterStatus === todo.status ) {
+              return (
+                <li>
+                  <h3 style={{display: 'inline-block', margin: '10px 10px 10px 0'}}>{todo.title}</h3>
+                  <select name="status" value={todo.status} onChange={e => changeStatus( todo.id, e.target.value )}>
+                    <option value="未着手">未着手</option>
+                    <option value="進行中">進行中</option>
+                    <option value="完了">完了</option>
+                  </select><br />
+                  {todo.desc && <p style={{margin: '0 0 10px 0'}}>{todo.desc}</p>}
+                  <button onClick={() => openEditor(todo)}>編集</button>
+                  <button onClick={() => deleteTodo(todo.id)}>削除</button>
+                  {
+                    editTodo.id === todo.id &&
+                    <div>
+                      <input name="newTodoTitle" value={editTodo.title} placeholder="タイトル" onChange={changeEditTodoTitle} /><p id="edit-error" style={{color: 'red', display: 'inline-block'}}></p><br />
+                      <textarea name="newTodoDesc" value={editTodo.desc} placeholder="詳細" onChange={changeEditTodoDesc}></textarea><br />
+                      <button onClick={completeEditTodo}>追加</button>
+                      <button onClick={closeEditor}>キャンセル</button>
+                    </div>
+                  }
+                </li>
+              );
+            } else {
+              return false;
+            }
           })
         }
       </ul>
